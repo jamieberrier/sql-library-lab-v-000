d3bc3dcaca80fae70ac40085e56765dd6f2039d9
jamieberrier <berrier.jamie@gmail.com>
2019-06-01 23:28:28 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -32,5 +32,6 @@ end
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(books.title) FROM characters
   JOIN character_books ON characters.id = character_books.character_id
+  GROUP BY characters.name
   JOIN books ON books.id = character_books.book_id"
 end
