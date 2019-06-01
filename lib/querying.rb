6c0e12360dc7555ed0060890a2d81676da63b915
jamieberrier <berrier.jamie@gmail.com>
2019-06-01 23:26:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -31,5 +31,5 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, SUM() FROM characters
-  JOIN books ON"
+  JOIN character_books ON characters.id = character_books.character_id"
 end
