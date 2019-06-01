9a2d3684fbf8afd31f1e9b36d009e69b18d80567
jamieberrier <berrier.jamie@gmail.com>
2019-06-01 23:24:01 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,7 +25,8 @@ end
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM series
   JOIN characters ON characters.series_id = series.id
-  GROUP BY characters.series_id"
+  GROUP BY characters.series_id
+  ORDER BY characters.series_id"
 end
 
 def select_character_names_and_number_of_books_they_are_in
